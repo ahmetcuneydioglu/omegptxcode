63a024cf7ae1b7b1efc09466bc994df124f5aa1a
ahmetcnd <canahmettt@gmail.com>
2026-03-01 23:12:09 +0300

    Initial Commit

---
+++ omegptnative/omegptnativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  omegptnativeApp.swift
+//  omegptnative
+//
+//  Created by AHMETCND on 1.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct omegptnativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
